606ae6e9c0b2c03219081fc9036bae589439e5b2
VRUSHABH SOMANI <vssomani2002@gmail.com>
2025-09-07 18:24:50 +0530

    imported signal components and its functionalities

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "./header/header";
 import { Footer } from './footer/footer';
+import { SignalEx } from './signal-ex/signal-ex';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Footer],
+  imports: [Header, Footer, SignalEx],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
