052cbc481e2f66ac98b47eeb3d511613588d5b45
VRUSHABH SOMANI <vssomani2002@gmail.com>
2025-09-08 22:54:57 +0530

    ngStyle explained

--- src/app/att-directive/att-directive.ts
+++ src/app/att-directive/att-directive.ts
@@ -3,14 +3,19 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-att-directive',
-  imports: [NgClass],
+  imports: [NgClass, NgStyle],
   templateUrl: './att-directive.html',
   styleUrl: './att-directive.css'
 })
 export class AttDirective {
   changeClass = signal<string>("");
+  changeColor:boolean=false;
 
   changeBG(name:string){
     this.changeClass.set(name);
+  }
+
+  changeCC(){
+    this.changeColor = !this.changeColor;
   }
 }
